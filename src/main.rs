88d33d98f52f1eb1ef6c8e5a30509700c6ccee76
yum <contact@meyume.com>
2026-01-18 16:40:28 -0500

    refactor: move proxy logic to struct

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,18 @@
-use std::sync::Arc;
-
 use anyhow::Context;
-use tokio::{fs::read_to_string, net::UdpSocket, time::Instant};
+use tokio::fs::read_to_string;
 
-use crate::config::Config;
+use crate::{config::Config, proxy::Proxy};
 
 mod config;
+mod proxy;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let config: Config = toml::from_str(&read_to_string("./lucifer.toml").await?)
         .context("Failed to read config")?;
 
-    let addr = format!("127.0.0.1:{}", config.proxy.port.unwrap_or(53));
-    let sock = Arc::new(UdpSocket::bind(&addr).await?);
-    let upstream_sock = Arc::new(UdpSocket::bind("0.0.0.0:0").await?);
-    upstream_sock
-        .connect(format!(
-            "{}:53",
-            config
-                .proxy
-                .nameservers
-                .first()
-                .expect("nameservers not found")
-        ))
-        .await?;
-    println!("DNS proxy running on {addr}");
+    let proxy = Proxy::new(config.proxy);
+    proxy.start().await?;
 
-    loop {
-        let mut buf = [0; 4096];
-        let start = Instant::now();
-        let (len, client_addr) = sock.recv_from(&mut buf).await?;
-        println!("Received request from {}", client_addr);
-
-        let cloudflare = upstream_sock.clone();
-        let proxy = sock.clone();
-        tokio::spawn(async move {
-            cloudflare.send(&buf[..len]).await.unwrap();
-            let (reply_len, _) = cloudflare.recv_from(&mut buf).await.unwrap();
-            proxy.send_to(&buf[..reply_len], client_addr).await.unwrap();
-            println!("DNS request took {}ms", start.elapsed().as_millis())
-        });
-    }
+    Ok(())
 }
---
+++ src/proxy.rs
@@ -0,0 +1,47 @@
+use std::sync::Arc;
+
+use tokio::{net::UdpSocket, time::Instant};
+
+use crate::config::ProxyConfig;
+
+pub struct Proxy {
+    config: ProxyConfig,
+}
+
+impl Proxy {
+    pub fn new(config: ProxyConfig) -> Self {
+        Self { config }
+    }
+
+    pub async fn start(&self) -> anyhow::Result<()> {
+        let addr = format!("127.0.0.1:{}", self.config.port.unwrap_or(53));
+        let sock = Arc::new(UdpSocket::bind(&addr).await?);
+        let upstream_sock = Arc::new(UdpSocket::bind("0.0.0.0:0").await?);
+        upstream_sock
+            .connect(format!(
+                "{}:53",
+                self.config
+                    .nameservers
+                    .first()
+                    .expect("nameservers not found")
+            ))
+            .await?;
+        println!("DNS proxy running on {addr}");
+
+        loop {
+            let mut buf = [0; 4096];
+            let start = Instant::now();
+            let (len, client_addr) = sock.recv_from(&mut buf).await?;
+            println!("Received request from {}", client_addr);
+
+            let cloudflare = upstream_sock.clone();
+            let proxy = sock.clone();
+            tokio::spawn(async move {
+                cloudflare.send(&buf[..len]).await.unwrap();
+                let (reply_len, _) = cloudflare.recv_from(&mut buf).await.unwrap();
+                proxy.send_to(&buf[..reply_len], client_addr).await.unwrap();
+                println!("DNS request took {}ms", start.elapsed().as_millis())
+            });
+        }
+    }
+}
