f0fb7cc178f3605a79893d49d6bacae8bc8c271a
yum <contact@meyume.com>
2026-01-18 17:45:15 -0500

    fix: ignore subdomains

--- src/dns.rs
+++ src/dns.rs
@@ -7,7 +7,9 @@ pub fn read_domain(buf: &[u8]) -> String {
         parts.push(str::from_utf8(part).expect("should be valid utf8"));
         i += length + 1;
     }
-    parts.join(".")
+
+    // naively just take the last two parts and join them for the domain
+    parts[parts.len() - 2..].join(".")
 }
 
 // buf is the buf containing the query. we will write to the same buf
@@ -35,5 +37,18 @@ mod tests {
         encoded.push(0);
         let actual = read_domain(&encoded);
         assert_eq!(input, actual);
+    }
+
+    #[test]
+    fn test_read_prefix_domain() {
+        let input = "www.test.github.com";
+        let mut encoded = vec![];
+        for part in input.split(".") {
+            encoded.push(part.len() as u8);
+            encoded.extend_from_slice(part.as_bytes());
+        }
+        encoded.push(0);
+        let actual = read_domain(&encoded);
+        assert_eq!("github.com", actual);
     }
 }
