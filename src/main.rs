15a451b99bdfa9cbda750c503dc4dd6106095050
yum <contact@meyume.com>
2026-03-17 19:43:13 -0400

    feat: add config as a cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::path::PathBuf;
+
 use anyhow::Context;
+use clap::{Parser, command};
 use tokio::fs::read_to_string;
 
 use crate::{config::Config, proxy::Proxy};
@@ -7,10 +10,19 @@ mod config;
 pub mod dns;
 mod proxy;
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long, value_name = "FILE")]
+    config: Option<PathBuf>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let config: Config = toml::from_str(&read_to_string("./lucifer.toml").await?)
-        .context("Failed to read config")?;
+    let args = Args::parse();
+    let config: Config =
+        toml::from_str(&read_to_string(args.config.unwrap_or("./lucifer.toml".into())).await?)
+            .context("Failed to read config")?;
 
     tracing_subscriber::fmt::init();
 
