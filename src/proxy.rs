3f974b0db3e6bcdac83f37146ebb8910e26d6fd8
yum <contact@meyume.com>
2026-03-17 16:55:25 -0400

    fix: log errors and fix lints

--- src/proxy.rs
+++ src/proxy.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
 
+use anyhow::anyhow;
 use tokio::{net::UdpSocket, time::Instant};
-use tracing::{debug, info, trace};
+use tracing::{debug, error, info, trace};
 
 use crate::{
     config::ProxyConfig,
@@ -18,18 +19,14 @@ impl Proxy {
     }
 
     pub async fn start(&self) -> anyhow::Result<()> {
+        let Some(nameserver) = self.config.nameservers.first() else {
+            return Err(anyhow!("No nameservers configured."));
+        };
+
         let addr = format!("127.0.0.1:{}", self.config.port.unwrap_or(53));
         let sock = Arc::new(UdpSocket::bind(&addr).await?);
         let upstream_sock = Arc::new(UdpSocket::bind("0.0.0.0:0").await?);
-        upstream_sock
-            .connect(format!(
-                "{}:53",
-                self.config
-                    .nameservers
-                    .first()
-                    .expect("nameservers not found")
-            ))
-            .await?;
+        upstream_sock.connect(format!("{}:53", nameserver)).await?;
         info!("DNS proxy running on {addr}");
 
         loop {
@@ -41,19 +38,31 @@ impl Proxy {
             let domain = read_domain(&buf[12..]);
             debug!("DNS query for {domain}");
             if self.config.blocklist.contains(&domain) {
-                info!("accessing blocked domain: {domain}");
+                info!("Accessing blocked domain: {domain}");
                 write_sinkhole_response(&mut buf);
-                sock.send_to(&buf[..len], client_addr).await.unwrap();
+                if let Err(e) = sock.send_to(&buf[..len], client_addr).await {
+                    error!("Failed to send response: {e}");
+                };
                 continue;
             }
 
             let upstream_sock = upstream_sock.clone();
             let sock = sock.clone();
             tokio::spawn(async move {
-                upstream_sock.send(&buf[..len]).await.unwrap();
-                let (reply_len, _) = upstream_sock.recv_from(&mut buf).await.unwrap();
-                sock.send_to(&buf[..reply_len], client_addr).await.unwrap();
-                trace!("DNS request took {}ms", start.elapsed().as_millis())
+                if let Err(e) = upstream_sock.send(&buf[..len]).await {
+                    error!("Failed to send request to upstream: {e}");
+                }
+                match upstream_sock.recv_from(&mut buf).await {
+                    Ok((reply_len, _)) => {
+                        if let Err(e) = sock.send_to(&buf[..reply_len], client_addr).await {
+                            error!("Failed to send response: {e}");
+                        }
+                        trace!("DNS request took {}ms", start.elapsed().as_millis())
+                    }
+                    Err(e) => {
+                        error!("Failed to get upstream response: {e}");
+                    }
+                }
             });
         }
     }
